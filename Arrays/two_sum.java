0c646f4fd31b1a7c3eeecd05689a173777e6c6ee
OneCODE2 <109349689+OneCODE2@users.noreply.github.com>
2024-07-24 23:29:09 +0530

    Create two_sum.java

---
+++ Arrays/two_sum.java
@@ -0,0 +1,17 @@
+
+among the three brute ,better, and optimal this is optimal and best soln using hash table  
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
+
